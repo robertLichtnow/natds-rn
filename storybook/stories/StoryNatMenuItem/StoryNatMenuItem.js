9320377f7a400e1f92d6bb3b5ea06eae21512bdf
Douglas Freitas <douglasofreitas@gmail.com>
2020-01-24 14:46:48 -0300

    fix tests

--- storybook/stories/StoryNatMenuItem/StoryNatMenuItem.js
+++ storybook/stories/StoryNatMenuItem/StoryNatMenuItem.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Text, ScrollView,  } from 'react-native';
+import { Text, ScrollView } from 'react-native';
 import { NatContainer, NatMenuItem } from '../../../lib';
 
 export const StoryNatMenuItem = () => {
